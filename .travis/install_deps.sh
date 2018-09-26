5319fc523ca35ceb42d662666259083016ff3a91
fjaviersanchez <francs1@uci.edu>
2018-09-26 13:43:03 -0700

    Adding anaconda

--- .travis/install_deps.sh
+++ .travis/install_deps.sh
@@ -2,7 +2,37 @@
 
 # If we are using OSX, then install fftw3 and gsl2
 
-if ! [[ $TRAVIS_OS_NAME == "linux" ]]; then brew install gcc; brew install fftw --with-openmp --without-fortran; brew install autotools; brew install autoconf; fi
+if ! [[ $TRAVIS_OS_NAME == "linux" ]]; then brew install gcc; brew install fftw --with-openmp --without-fortran; brew install autotools; brew install autoconf; 
+
+    # Install some custom requirements on OS X
+    if test -e $HOME/miniconda/bin; then
+      echo "miniconda already installed.";
+    else
+      echo "Installing miniconda.";
+      rm -rf $HOME/miniconda;
+      mkdir -p $HOME/download;
+      if [ "${TOXENV}" = py27 ]; then
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O $HOME/download/miniconda.sh;
+      else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/download/miniconda.sh;
+      fi;
+      bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
+    fi;
+    source $HOME/miniconda/bin/activate
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda info -a
+
+    case "${TOXENV}" in
+        py27)
+            conda create -q -n test-environment python=2.7 pip
+            ;;
+        py36)
+            conda create -q -n test-environment python=3.6 pip
+            ;;
+    esac;
+fi
 
 # Install chealpix
 
