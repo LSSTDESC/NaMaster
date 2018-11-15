a19721ec7024740313f05b8ac9e55e58f5880499
Daniel Lenz <mail@daniellenz.org>
2018-11-14 17:14:51 -0800

    Black formatting

--- pymaster/bins.py
+++ pymaster/bins.py
@@ -1,7 +1,8 @@
 from pymaster import nmtlib as lib
 import numpy as np
 
-class NmtBin(object) :
+
+class NmtBin(object):
     """
     An NmtBin object defines the set of bandpowers used in the computation of the pseudo-Cl estimator. The definition of bandpowers is described in Section 3.6 of the scientific documentation.
 
@@ -12,34 +13,36 @@ class NmtBin(object) :
     :param int nlb: integer value corresponding to a constant bandpower width. I.e. the bandpowers will be defined as consecutive sets of nlb multipoles from l=2 to l=lmax (see below) with equal weights. If this argument is provided, the values of ells, bpws and weights are ignored.
     :param int lmax: integer value corresponding to the maximum multiple used by these bandpowers. If None, it will be set to 3*nside-1. In any case the actual maximum multipole will be chosen as the minimum of lmax, 3*nside-1 and the maximum element of ells.
     """
-    def __init__(self,nside,bpws=None,ells=None,weights=None,nlb=None,lmax=None) :
-        self.bin=None
-        
-        if((bpws is None) and (ells is None) and (weights is None) and (nlb is None)) :
+
+    def __init__(self, nside, bpws=None, ells=None, weights=None, nlb=None, lmax=None):
+        self.bin = None
+
+        if (bpws is None) and (ells is None) and (weights is None) and (nlb is None):
             raise KeyError("Must supply bandpower arrays or constant bandpower width")
 
-        if(lmax is None) :
-            lmax_in=3*nside-1
-        else :
-            lmax_in=lmax
+        if lmax is None:
+            lmax_in = 3 * nside - 1
+        else:
+            lmax_in = lmax
 
-        if(nlb is None) :
-            if((bpws is None) and (ells is None) and (weights is None)) :
+        if nlb is None:
+            if (bpws is None) and (ells is None) and (weights is None):
                 raise KeyError("Must provide bpws, ells and weights")
-            ell_max=min(3*nside-1,lmax_in)
-            self.bin=lib.bins_create_py(bpws.astype(np.int32),ells.astype(np.int32),weights,
-                                        int(ell_max))
-        else :
-            ell_max=min(3*nside-1,lmax_in)
-            self.bin=lib.bins_constant(nlb,ell_max)
-        self.lmax=ell_max
+            ell_max = min(3 * nside - 1, lmax_in)
+            self.bin = lib.bins_create_py(
+                bpws.astype(np.int32), ells.astype(np.int32), weights, int(ell_max)
+            )
+        else:
+            ell_max = min(3 * nside - 1, lmax_in)
+            self.bin = lib.bins_constant(nlb, ell_max)
+        self.lmax = ell_max
 
-    def __del__(self) :
-        if self.bin is not None :
+    def __del__(self):
+        if self.bin is not None:
             lib.bins_free(self.bin)
-            self.bin=None
+            self.bin = None
 
-    def get_n_bands(self) :
+    def get_n_bands(self):
         """
         Returns the number of bandpowers stored in this object
 
@@ -47,96 +50,97 @@ class NmtBin(object) :
         """
         return self.bin.n_bands
 
-    def get_nell_list(self) :
+    def get_nell_list(self):
         """
         Returns an array with the number of multipoles in each bandpower stored in this object
 
         :return: number of multipoles per bandpower
         """
-        return lib.get_nell_list(self.bin,self.bin.n_bands)
+        return lib.get_nell_list(self.bin, self.bin.n_bands)
 
-    def get_ell_list(self,ibin) :
+    def get_ell_list(self, ibin):
         """
         Returns an array with the multipoles in the ibin-th bandpower
 
         :param int ibin: bandpower index
         :return: multipoles associated with bandpower ibin
         """
-        return lib.get_ell_list(self.bin,ibin,lib.get_nell(self.bin,ibin))
+        return lib.get_ell_list(self.bin, ibin, lib.get_nell(self.bin, ibin))
 
-    def get_weight_list(self,ibin) :
+    def get_weight_list(self, ibin):
         """
         Returns an array with the weights associated to each multipole in the ibin-th bandpower
 
         :param int ibin: bandpower index
         :return: weights associated to multipoles in bandpower ibin
         """
-        return lib.get_weight_list(self.bin,ibin,lib.get_nell(self.bin,ibin))
+        return lib.get_weight_list(self.bin, ibin, lib.get_nell(self.bin, ibin))
 
-    def get_effective_ells(self) :
+    def get_effective_ells(self):
         """
         Returns an array with the effective multipole associated to each bandpower. These are computed as a weighted average of the multipoles within each bandpower.
 
         :return: effective multipoles for each bandpower
         """
-        return lib.get_ell_eff(self.bin,self.bin.n_bands)
+        return lib.get_ell_eff(self.bin, self.bin.n_bands)
 
-    def bin_cell(self,cls_in) :
+    def bin_cell(self, cls_in):
         """
         Bins a power spectrum into bandpowers. This is carried out as a weighted average over the multipoles in each bandpower.
 
         :param array-like cls_in: 2D array of power spectra
         :return: array of bandpowers
         """
-        oned=False
-        if(cls_in.ndim!=2) :
-            oned=True
-            cls_in=np.array([cls_in])
-        if((cls_in.ndim>2) or (len(cls_in[0])!=self.lmax+1)) :
+        oned = False
+        if cls_in.ndim != 2:
+            oned = True
+            cls_in = np.array([cls_in])
+        if (cls_in.ndim > 2) or (len(cls_in[0]) != self.lmax + 1):
             raise ValueError("Input Cl has wrong size")
-        cl1d=lib.bin_cl(self.bin,cls_in,len(cls_in)*self.bin.n_bands)
-        clout=np.reshape(cl1d,[len(cls_in),self.bin.n_bands])
-        if oned :
-            clout=clout[0]
+        cl1d = lib.bin_cl(self.bin, cls_in, len(cls_in) * self.bin.n_bands)
+        clout = np.reshape(cl1d, [len(cls_in), self.bin.n_bands])
+        if oned:
+            clout = clout[0]
         return clout
 
-    def unbin_cell(self,cls_in) :
+    def unbin_cell(self, cls_in):
         """
         Un-bins a set of bandpowers into a power spectrum. This is simply done by assigning a constant value for every multipole in each bandpower (corresponding to the value of that bandpower).
 
         :param array-like cls_in: array of bandpowers
         :return: array of power spectra
         """
-        oned=False
-        if(cls_in.ndim!=2) :
-            oned=True
-            cls_in=np.array([cls_in])
-        if((cls_in.ndim>2) or (len(cls_in[0])!=self.bin.n_bands)) :
+        oned = False
+        if cls_in.ndim != 2:
+            oned = True
+            cls_in = np.array([cls_in])
+        if (cls_in.ndim > 2) or (len(cls_in[0]) != self.bin.n_bands):
             raise ValueError("Input Cl has wrong size")
-        cl1d=lib.unbin_cl(self.bin,cls_in,len(cls_in)*(self.lmax+1))
-        clout=np.reshape(cl1d,[len(cls_in),self.lmax+1])
-        if oned :
-            clout=clout[0]
+        cl1d = lib.unbin_cl(self.bin, cls_in, len(cls_in) * (self.lmax + 1))
+        clout = np.reshape(cl1d, [len(cls_in), self.lmax + 1])
+        if oned:
+            clout = clout[0]
         return clout
 
 
-class NmtBinFlat(object) :
+class NmtBinFlat(object):
     """
     An NmtBinFlat object defines the set of bandpowers used in the computation of the pseudo-Cl estimator. The definition of bandpowers is described in Section 3.6 of the scientific documentation. Note that currently pymaster only supports top-hat bandpowers for flat-sky power spectra.
 
     :param array-like l0: array of floats corresponding to the lower bound of each bandpower.
     :param array-like lf: array of floats corresponding to the upper bound of each bandpower. lf should have the same shape as l0
     """
-    def __init__(self,l0,lf) :
-        self.bin=None
-        self.bin=lib.bins_flat_create_py(l0,lf)
 
-    def __del__(self) :
-        if self.bin is not None :
+    def __init__(self, l0, lf):
+        self.bin = None
+        self.bin = lib.bins_flat_create_py(l0, lf)
+
+    def __del__(self):
+        if self.bin is not None:
             lib.bins_flat_free(self.bin)
-            self.bin=None
+            self.bin = None
 
-    def get_n_bands(self) :
+    def get_n_bands(self):
         """
         Returns the number of bandpowers stored in this object
 
@@ -144,15 +148,15 @@ class NmtBinFlat(object) :
         """
         return self.bin.n_bands
 
-    def get_effective_ells(self) :
+    def get_effective_ells(self):
         """
         Returns an array with the effective multipole associated to each bandpower. These are computed as a weighted average of the multipoles within each bandpower.
 
         :return: effective multipoles for each bandpower
         """
-        return lib.get_ell_eff_flat(self.bin,self.bin.n_bands)
+        return lib.get_ell_eff_flat(self.bin, self.bin.n_bands)
 
-    def bin_cell(self,ells,cls_in) :
+    def bin_cell(self, ells, cls_in):
         """
         Bins a power spectrum into bandpowers. This is carried out as a weighted average over the multipoles in each bandpower.
 
@@ -160,19 +164,19 @@ class NmtBinFlat(object) :
         :param array-like cls_in: 2D array of input power spectra
         :return: array of bandpowers
         """
-        oned=False
-        if(cls_in.ndim!=2) :
-            oned=True
-            cls_in=np.array([cls_in])
-        if((cls_in.ndim>2) or (len(cls_in[0])!=len(ells))) :
+        oned = False
+        if cls_in.ndim != 2:
+            oned = True
+            cls_in = np.array([cls_in])
+        if (cls_in.ndim > 2) or (len(cls_in[0]) != len(ells)):
             raise ValueError("Input Cl has wrong size")
-        cl1d=lib.bin_cl_flat(self.bin,ells,cls_in,len(cls_in)*self.bin.n_bands)
-        clout=np.reshape(cl1d,[len(cls_in),self.bin.n_bands])
-        if oned :
-            clout=clout[0]
+        cl1d = lib.bin_cl_flat(self.bin, ells, cls_in, len(cls_in) * self.bin.n_bands)
+        clout = np.reshape(cl1d, [len(cls_in), self.bin.n_bands])
+        if oned:
+            clout = clout[0]
         return clout
 
-    def unbin_cell(self,cls_in,ells) :
+    def unbin_cell(self, cls_in, ells):
         """
         Un-bins a set of bandpowers into a power spectrum. This is simply done by assigning a constant value for every multipole in each bandpower (corresponding to the value of that bandpower).
 
@@ -180,14 +184,14 @@ class NmtBinFlat(object) :
         :param array-like ells: array of multipoles at which the power spectra should be intepolated
         :return: array of power spectra
         """
-        oned=False
-        if(cls_in.ndim!=2) :
-            oned=True
-            cls_in=np.array([cls_in])
-        if((cls_in.ndim>2) or (len(cls_in[0])!=self.bin.n_bands)) :
+        oned = False
+        if cls_in.ndim != 2:
+            oned = True
+            cls_in = np.array([cls_in])
+        if (cls_in.ndim > 2) or (len(cls_in[0]) != self.bin.n_bands):
             raise ValueError("Input Cl has wrong size")
-        cl1d=lib.unbin_cl_flat(self.bin,cls_in,ells,len(cls_in)*len(ells))
-        clout=np.reshape(cl1d,[len(cls_in),len(ells)])
-        if oned :
-            clout=clout[0]
+        cl1d = lib.unbin_cl_flat(self.bin, cls_in, ells, len(cls_in) * len(ells))
+        clout = np.reshape(cl1d, [len(cls_in), len(ells)])
+        if oned:
+            clout = clout[0]
         return clout
--- pymaster/covariance.py
+++ pymaster/covariance.py
@@ -1,104 +1,109 @@
 from pymaster import nmtlib as lib
 import numpy as np
 
-class NmtCovarianceWorkspace(object) :
+
+class NmtCovarianceWorkspace(object):
     """
     NmtCovarianceWorkspace objects are used to compute and store the coupling coefficients needed to calculate the Gaussian covariance matrix under the Efstathiou approximation (astro-ph/0307515). When initialized, this object is practically empty. The information describing the coupling coefficients must be computed or read from a file afterwards.
     """
-    def __init__(self) :
-        self.wsp=None
-    
-    def __del__(self) :
-        if(self.wsp is not None) :
-            lib.covar_workspace_free(self.wsp)
-            self.wsp=None
 
-    def read_from(self,fname) :
+    def __init__(self):
+        self.wsp = None
+
+    def __del__(self):
+        if self.wsp is not None:
+            lib.covar_workspace_free(self.wsp)
+            self.wsp = None
+
+    def read_from(self, fname):
         """
         Reads the contents of an NmtCovarianceWorkspace object from a file (encoded using an internal binary format).
 
         :param str fname: input file name
         """
-        if self.wsp is not None :
+        if self.wsp is not None:
             lib.covar_workspace_free(self.wsp)
-            self.wsp=None
-        self.wsp=lib.read_covar_workspace(fname);
+            self.wsp = None
+        self.wsp = lib.read_covar_workspace(fname)
 
-    def compute_coupling_coefficients(self,wa,wb) :
+    def compute_coupling_coefficients(self, wa, wb):
         """
         Computes coupling coefficients of the Gaussian covariance between the power spectra computed using wa and wb (two NmtWorkspace objects).
 
         :param NmtWorkspace wa,wb: workspaces used to compute the two power spectra whose covariance matrix you want to compute.
         """
-        if self.wsp is not None :
+        if self.wsp is not None:
             lib.covar_workspace_free(self.wsp)
-            self.wsp=None
-        ns=wa.wsp.nside;
-        if(wa.wsp.nside!=wb.wsp.nside) :
+            self.wsp = None
+        ns = wa.wsp.nside
+        if wa.wsp.nside != wb.wsp.nside:
             raise ValueError("Everything should have the same resolution!")
-        if((wa.wsp.ncls!=1) or (wb.wsp.ncls!=1)) :
+        if (wa.wsp.ncls != 1) or (wb.wsp.ncls != 1):
             raise ValueError("Gaussian covariances only supported for spin-0 fields")
-        self.wsp=lib.covar_workspace_init_py(wa.wsp,wb.wsp)
+        self.wsp = lib.covar_workspace_init_py(wa.wsp, wb.wsp)
 
-    def write_to(self,fname) :
+    def write_to(self, fname):
         """
         Writes the contents of an NmtCovarianceWorkspace object to a file (encoded using an internal binary format).
 
         :param str fname: output file name
         """
-        if self.wsp is None :
+        if self.wsp is None:
             raise ValueError("Must initialize workspace before writing")
-        lib.write_covar_workspace(self.wsp,fname)
+        lib.write_covar_workspace(self.wsp, fname)
 
-class NmtCovarianceWorkspaceFlat(object) :
+
+class NmtCovarianceWorkspaceFlat(object):
     """
     NmtCovarianceWorkspaceFlat objects are used to compute and store the coupling coefficients needed to calculate the Gaussian covariance matrix under a flat-sky version the Efstathiou approximation (astro-ph/0307515). When initialized, this object is practically empty. The information describing the coupling coefficients must be computed or read from a file afterwards.
     """
-    def __init__(self) :
-        self.wsp=None
-    
-    def __del__(self) :
-        if(self.wsp is not None) :
-            lib.covar_workspace_flat_free(self.wsp)
-            self.wsp=None
 
-    def read_from(self,fname) :
+    def __init__(self):
+        self.wsp = None
+
+    def __del__(self):
+        if self.wsp is not None:
+            lib.covar_workspace_flat_free(self.wsp)
+            self.wsp = None
+
+    def read_from(self, fname):
         """
         Reads the contents of an NmtCovarianceWorkspaceFlat object from a file (encoded using an internal binary format).
 
         :param str fname: input file name
         """
-        if self.wsp is not None :
+        if self.wsp is not None:
             lib.covar_workspace_flat_free(self.wsp)
-            self.wsp=None
-        self.wsp=lib.read_covar_workspace_flat(fname);
+            self.wsp = None
+        self.wsp = lib.read_covar_workspace_flat(fname)
 
-    def compute_coupling_coefficients(self,wa,wb) :
+    def compute_coupling_coefficients(self, wa, wb):
         """
         Computes coupling coefficients of the Gaussian covariance between the power spectra computed using wa and wb (two NmtWorkspaceFlat objects).
 
         :param NmtWorkspaceFlat wa,wb: workspaces used to compute the two power spectra whose covariance matrix you want to compute.
         """
-        if((wa.wsp.fs.nx!=wb.wsp.fs.nx) or (wa.wsp.fs.ny!=wb.wsp.fs.ny)) :
+        if (wa.wsp.fs.nx != wb.wsp.fs.nx) or (wa.wsp.fs.ny != wb.wsp.fs.ny):
             raise ValueError("Everything should have the same resolution!")
-        if((wa.wsp.ncls!=1) or (wb.wsp.ncls!=1)) :
+        if (wa.wsp.ncls != 1) or (wb.wsp.ncls != 1):
             raise ValueError("Gaussian covariances only supported for spin-0 fields")
-        if self.wsp is not None :
+        if self.wsp is not None:
             lib.covar_workspace_flat_free(self.wsp)
-            self.wsp=None
-        self.wsp=lib.covar_workspace_flat_init_py(wa.wsp,wb.wsp)
+            self.wsp = None
+        self.wsp = lib.covar_workspace_flat_init_py(wa.wsp, wb.wsp)
 
-    def write_to(self,fname) :
+    def write_to(self, fname):
         """
         Writes the contents of an NmtCovarianceWorkspaceFlat object to a file (encoded using an internal binary format).
 
         :param str fname: output file name
         """
-        if self.wsp is None :
+        if self.wsp is None:
             raise ValueError("Must initialize workspace before writing")
-        lib.write_covar_workspace_flat(self.wsp,fname)
+        lib.write_covar_workspace_flat(self.wsp, fname)
 
-def gaussian_covariance(cw,cla1b1,cla1b2,cla2b1,cla2b2) :
+
+def gaussian_covariance(cw, cla1b1, cla1b2, cla2b1, cla2b2):
     """
     Computes Gaussian covariance matrix for power spectra using the information precomputed in cw (a NmtCovarianceWorkspace object). cw should have been initialized using the two NmtWorkspace objects used to compute these power spectra. The notation above assumes that these two NmtWorkspace objects, wa and wb, were used to compute the power spectra of four fields: a1 and a2 for wa, b1 and b2 for wb. Then, claXbY above should be a prediction for the power spectrum between fields aX and bY. These predicted input power spectra should be defined for all ells <=3*nside (where nside is the HEALPix resolution parameter of the fields that were correlated).
 
@@ -108,16 +113,24 @@ def gaussian_covariance(cw,cla1b1,cla1b2,cla2b1,cla2b2) :
     :param cla2b1: prediction for the cross-power spectrum between a2 and b1.
     :param cla2b2: prediction for the cross-power spectrum between a2 and b2.
     """
-    if((len(cla1b1)!=cw.wsp.lmax_a+1) or (len(cla1b2)!=cw.wsp.lmax_a+1) or (len(cla2b1)!=cw.wsp.lmax_a+1) or (len(cla2b2)!=cw.wsp.lmax_a+1)) :
+    if (
+        (len(cla1b1) != cw.wsp.lmax_a + 1)
+        or (len(cla1b2) != cw.wsp.lmax_a + 1)
+        or (len(cla2b1) != cw.wsp.lmax_a + 1)
+        or (len(cla2b2) != cw.wsp.lmax_a + 1)
+    ):
         raise ValueError("Input C_ls have a weird length")
-    len_a=cw.wsp.ncls_a*cw.wsp.bin_a.n_bands
-    len_b=cw.wsp.ncls_b*cw.wsp.bin_b.n_bands
+    len_a = cw.wsp.ncls_a * cw.wsp.bin_a.n_bands
+    len_b = cw.wsp.ncls_b * cw.wsp.bin_b.n_bands
 
-    covar1d=lib.comp_gaussian_covariance(cw.wsp,cla1b1,cla1b2,cla2b1,cla2b2,len_a*len_b)
-    covar=np.reshape(covar1d,[len_a,len_b])
+    covar1d = lib.comp_gaussian_covariance(
+        cw.wsp, cla1b1, cla1b2, cla2b1, cla2b2, len_a * len_b
+    )
+    covar = np.reshape(covar1d, [len_a, len_b])
     return covar
 
-def gaussian_covariance_flat(cw,larr,cla1b1,cla1b2,cla2b1,cla2b2) :
+
+def gaussian_covariance_flat(cw, larr, cla1b1, cla1b2, cla2b1, cla2b2):
     """
     Computes Gaussian covariance matrix for flat-sky power spectra using the information precomputed in cw (a NmtCovarianceWorkspaceFlat object). cw should have been initialized using the two NmtWorkspaceFlat objects used to compute these power spectra. The notation above assumes that these two NmtWorkspaceFlat objects, wa and wb, were used to compute the power spectra of four fields: a1 and a2 for wa, b1 and b2 for wb. Then, claXbY above should be a prediction for the power spectrum between fields aX and bY. These predicted input power spectra should be defined in a sufficiently well sampled range of ells given themap properties from which the power spectra were computed. The values of ell at which they are sampled are given by larr.
 
@@ -128,11 +141,18 @@ def gaussian_covariance_flat(cw,larr,cla1b1,cla1b2,cla2b1,cla2b2) :
     :param cla2b1: prediction for the cross-power spectrum between a2 and b1.
     :param cla2b2: prediction for the cross-power spectrum between a2 and b2.
     """
-    if((len(cla1b1)!=len(larr)) or (len(cla1b2)!=len(larr)) or (len(cla2b1)!=len(larr)) or (len(cla2b2)!=len(larr))) :
+    if (
+        (len(cla1b1) != len(larr))
+        or (len(cla1b2) != len(larr))
+        or (len(cla2b1) != len(larr))
+        or (len(cla2b2) != len(larr))
+    ):
         raise ValueError("Input C_ls have a weird length")
-    len_a=cw.wsp.ncls_a*cw.wsp.bin.n_bands
-    len_b=cw.wsp.ncls_b*cw.wsp.bin.n_bands
+    len_a = cw.wsp.ncls_a * cw.wsp.bin.n_bands
+    len_b = cw.wsp.ncls_b * cw.wsp.bin.n_bands
 
-    covar1d=lib.comp_gaussian_covariance_flat(cw.wsp,larr,cla1b1,cla1b2,cla2b1,cla2b2,len_a*len_b)
-    covar=np.reshape(covar1d,[len_a,len_b])
+    covar1d = lib.comp_gaussian_covariance_flat(
+        cw.wsp, larr, cla1b1, cla1b2, cla2b1, cla2b2, len_a * len_b
+    )
+    covar = np.reshape(covar1d, [len_a, len_b])
     return covar
