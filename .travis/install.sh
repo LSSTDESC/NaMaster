9cca4e1e287d74235fd61ee7893bc92e6e1957fe
David Alonso <dam.phys@gmail.com>
2019-12-24 19:58:32 +0000

    ping

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRAVIS_BUILD_DIR/lib:/usr/local/lib
 export LDFLAGS="-L$TRAVIS_BUILD_DIR/lib -L/usr/local/lib"
 export CPPFLAGS="-I$TRAVIS_BUILD_DIR/include -I/usr/local/include -fopenmp"
-#export CFLAGS="-fopenmp"
+export CFLAGS="-fopenmp"
 
 ./scripts/install_libnmt.sh $@
 make check
