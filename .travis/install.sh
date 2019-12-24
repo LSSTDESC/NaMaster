499ce061832503308e12567a8bddfe4601c5ccd9
David Alonso <dam.phys@gmail.com>
2019-12-24 20:03:43 +0000

    ping

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,8 +2,8 @@
 
 #### Install NaMaster C ####
 
-#export PATH=$TRAVIS_BUILD_DIR/bin:$PATH
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRAVIS_BUILD_DIR/lib:/usr/local/lib
+export PATH=$TRAVIS_BUILD_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRAVIS_BUILD_DIR/lib:/usr/local/lib
 export LDFLAGS="-L$TRAVIS_BUILD_DIR/lib -L/usr/local/lib"
 export CPPFLAGS="-I$TRAVIS_BUILD_DIR/include -I/usr/local/include -fopenmp"
 export CFLAGS="-fopenmp"
