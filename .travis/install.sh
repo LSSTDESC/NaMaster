447c6d6e8d8b91a2c187357283cad233e7e27503
David Alonso <dam.phys@gmail.com>
2019-12-25 08:07:20 +0000

    better

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,14 +8,5 @@ export LDFLAGS="-L$TRAVIS_BUILD_DIR/lib -L/usr/local/lib"
 export CPPFLAGS="-I$TRAVIS_BUILD_DIR/include -I/usr/local/include -fopenmp"
 export CFLAGS="-fopenmp"
 
-ADEPDIR=$PWD/_deps
-CPPFLAGS+=" -I${ADEPDIR}/include" LDFLAGS+=" -L${ADEPDIR}/lib" ./configure --prefix=${ADEPDIR} --with-pic $@
-make clean
-make
-make install
+./scripts/install_libnmt.sh
 make check
-#./configure --prefix=$TRAVIS_BUILD_DIR $@
-#make clean
-#make
-#sudo make install
-#make check
