be0564e482d91868c440689056541ead4ce2e74d
David Alonso <dam.phys@gmail.com>
2020-01-14 10:30:34 +0000

    cfitsio

--- .travis/install_deps.sh
+++ .travis/install_deps.sh
@@ -8,6 +8,9 @@ wget https://sourceforge.net/projects/healpix/files/Healpix_3.11/autotools_packa
 
 pip install nose healpy scipy
 
+# Install cfitsio
+wget heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz && tar xzf heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz && cd cfitsio-3.47 && ./configure && make && sudo make install && cd ..
+
 #### Install GSL2.0+ ####
 
 wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.5.tar.gz --passive-ftp && tar xzf gsl-2.5.tar.gz && cd gsl-2.5 &&  ./configure --enable-shared && make && sudo make install && cd ..
