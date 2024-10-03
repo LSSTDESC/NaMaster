7d00368e3b8098a2d10aa91c9e44ec64547eab33
damonge <dam.phys@gmail.com>
2024-10-03 15:31:13 +0100

    more typos

--- pymaster/nmtlib.py
+++ pymaster/nmtlib.py
@@ -10,7 +10,7 @@ if _swig_python_version_info < (2, 7, 0):
 
 # Import the low-level C/C++ module
 if __package__ or "." in __name__:
-    from . import _nmtlib
+    import _nmtlib
 else:
     import _nmtlib
 
